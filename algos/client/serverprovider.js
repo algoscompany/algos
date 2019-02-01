87a7a6c482e3c2be7fd73b68a5b634909ffe783a
algoscompany <47024098+algoscompany@users.noreply.github.com>
2019-02-01 08:18:57 +0100

    Update serverprovider.js

--- algos/client/serverprovider.js
+++ algos/client/serverprovider.js
@@ -8,27 +8,19 @@ function getResource(funcName, jsonToSend) {
 	return new Promise(function(resolve, reject) {
 		var httpReq = new XMLHttpRequest();
 		httpReq.onreadystatechange = function() {
-			var data;
-			if (httpReq.readyState == 4) {
-				if (httpReq.status == 200) {
-					data = JSON.parse(httpReq.responseText);
-					resolve(data);
-				} else {
-					reject(new Error(httpReq.statusText));
-				}
+			if (httpReq.readyState == 4 && httpReq.status == 200) {
+				resolve(data);	
+			}else{
+				reject(new Error(httpReq.statusText));
 			}
 		};
-    if(jsonToSend == null){
+	    if(jsonToSend == null){
 		  httpReq.open("GET", getFuncUrl() , true);
 		  httpReq.send();
-    }
-    else{
-      httpReq.open("POST", getFuncUrl() , true);
-      httpReq.send("json=" + jsonToSend);
-    }
+	    }
+	    else{
+	      httpReq.open("POST", getFuncUrl() , true);
+	      httpReq.send("json=" + jsonToSend);
+	    }
 	});
-}
-
-function getResource(funcName){
-  return getResource(funcName, null);
 }
