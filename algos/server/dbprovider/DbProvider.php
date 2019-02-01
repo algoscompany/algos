90a78db701290b2b9b2c73654945ead9e9e1b2f2
A33809 <A33809@S42-12.buzzi.local>
2019-02-01 09:21:07 +0100

    push dbprovider

--- algos/server/dbprovider/DbProvider.php
+++ algos/server/dbprovider/DbProvider.php
@@ -3,22 +3,37 @@ namespace algos\server\dbprovider;
 
 use algos\server\entity\Entity;
 
-class DbProvider implements DbProviderInterface{
-    public function select(Entity $T): Entity
-    {}
+class DbProvider implements DbProviderInterface {
+    /**Provider per il linguaggio MySQL*/
 
-    public function save(Entity $obj): bool
-    {}
-
-    public function update(Entity $obj): bool
-    {}
-
-    public function delete(Entity $obj): Entity
-    {}
-
-    public function saveOrUpdate(Entity $obj): bool
-    {}
-
+    public function select(Entity $T): Entity {
+        $var = "";
+        
+    }
     
+    public function select(Entity $T, array $whereclause): array {
+        
+    }
+
+    public function save(Entity $obj): bool {
+
+    }
+
+    public function update(Entity $obj): bool {
+
+    }
+
+    public function saveOrUpdate(Entity $obj): bool {
+        if($this->update($obj))
+            return true;
+        else if($this->save($obj))
+            return true;
+        else
+            return false;
+    }
+
+    public function delete(Entity $obj): Entity {
+
+    }
 }
 
