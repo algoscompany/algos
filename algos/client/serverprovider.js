58b5dba01ed9325d7f12aca82b6c65586a83aa41
algoscompany <47024098+algoscompany@users.noreply.github.com>
2019-02-28 19:19:15 +0100

    Update serverprovider.js
    aggiunta timeout e status error.

--- algos/client/serverprovider.js
+++ algos/client/serverprovider.js
@@ -1,26 +1,44 @@
-const urlHost = "algos.altervista.org/server/gateway/Gateway.php";
+const urlHost = "api.php";
+const timeout = 3000;	//3 sec
 
 function getFuncUrl(funcName){
   return urlHost + "?funcName=" + funcName;
 }
 
 function getResource(funcName, jsonToSend) {
-	return new Promise(function(resolve, reject) {
-		var httpReq = new XMLHttpRequest();
-		httpReq.onreadystatechange = function() {
+	return new Promise((resolve, reject) => {
+		let httpReq = new XMLHttpRequest();
+		
+		setTimeout(function(){
+			reject('408 Request Timeout');
+		}, timeout);
+
+		httpReq.onload = function(){
 			if (httpReq.readyState == 4 && httpReq.status == 200) {
-				resolve(data);	
+				resolve(httpReq.responseText);	
 			}else{
-				reject(new Error(httpReq.statusText));
+				reject(new Error(httpReq.status + " " + httpReq.statusText));
 			}
-		};
-	    if(jsonToSend == null){
-		  httpReq.open("GET", getFuncUrl() , true);
-		  httpReq.send();
-	    }
-	    else{
-	      httpReq.open("POST", getFuncUrl() , true);
-	      httpReq.send("json=" + jsonToSend);
-	    }
+		}
+		httpReq.onerror = function(){
+			reject(Error("Network Error"));
+		}
+		
+		if(jsonToSend == null){
+			httpReq.open("GET", getFuncUrl(funcName), true);
+			httpReq.send();
+		}
+		else{
+		  httpReq.open("POST", getFuncUrl(funcName), true);
+		  httpReq.send("json=" + jsonToSend);
+		}
+		
 	});
 }
+
+/*
+//esempio di invocazione senza json
+getResource("prova")
+  .then(func1)    //nome della funzione da eseguire una volta restituito il risultato
+  .catch(errFunc) //nome della funzione da eseguire in caso di errore
+*/
