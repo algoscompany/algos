bf462e438486cf1e197fb346186423439614dbd9
tommysasso <sassoli.tom21@gmail.com>
2019-03-01 08:19:58 +0100

    added admin e user gateway

--- algos/server/gateway/gateway.php
+++ algos/server/gateway/gateway.php
@@ -1,14 +1,11 @@
 <?php
 
-
+include_once __DIR__.'usergateway.php';
+include_once __DIR__.'administratorgateway.php';
 
 if(!isset($_GET['funcName'])){
 	header('HTTP/1.1 400 Bad Request');
 	die();
-}
-
-function getPasswordToken($param) {
-    ;
 }
 
 if(function_exists($_GET['funcName'])){
